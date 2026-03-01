e3f10a8542dff2a727d92e057127d51859eaacc6
nischay <rocknischay@gmail.com>
2026-03-01 12:23:02 +0530

    updated config

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,9 @@ export default defineConfig({
     // baseURL: '/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     // viewport: { width: 1920, height: 1080 }
   },
 
